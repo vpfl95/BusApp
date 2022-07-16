a1605743373a9403dece635ac5df121dbf85312a
vpfl95 <k_dh610@naver.com>
2022-07-17 03:25:51 +0900

    좌표얻기, 파싱하기

--- app/src/main/java/com/example/busapp/MainActivity.java
+++ app/src/main/java/com/example/busapp/MainActivity.java
@@ -6,7 +6,10 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +24,12 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.Locale;
 
 public class MainActivity extends Activity {
@@ -28,6 +37,9 @@ public class MainActivity extends Activity {
     private TextView mTextView;
     private ActivityMainBinding binding;
 
+    private String data;
+    private EditText edit;
+    private TextView buslist;
     private TextView txtLocation;
     private Button button;
     private FusedLocationProviderClient mFusedLocationClient;
@@ -38,6 +50,7 @@ public class MainActivity extends Activity {
     private StringBuilder stringBuilder;
     private boolean isContinue = false;
 
+    private TagoThread tagoThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,8 +58,8 @@ public class MainActivity extends Activity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mTextView = binding.text;
 
+        this.buslist = (TextView) findViewById(R.id.buslist);
         this.txtLocation = (TextView) findViewById(R.id.txtLocation);
         this.button = (Button) findViewById(R.id.button);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
@@ -72,7 +85,7 @@ public class MainActivity extends Activity {
                             stringBuilder.append("-");
                             stringBuilder.append(wayLongitude);
                             stringBuilder.append("\n\n");
-                            mTextView.setText(stringBuilder.toString());
+                            txtLocation.setText(stringBuilder.toString());
                         }
                         if (!isContinue && mFusedLocationClient != null) {
                             mFusedLocationClient.removeLocationUpdates(locationCallback);
@@ -82,11 +95,36 @@ public class MainActivity extends Activity {
             }
         };
 
-        button.setOnClickListener(view -> {
-            isContinue = true;
-            stringBuilder = new StringBuilder();
-            getLocation();
+//        button.setOnClickListener(view -> {
+//            isContinue = true;
+//            stringBuilder = new StringBuilder();
+//            getLocation();
+//            data = getTagoXmlData(wayLatitude,wayLongitude);
+//            buslist.setText(data);
+//        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tagoThread = new TagoThread();
+                tagoThread.start();
+                getLocation();
+                data = tagoThread.getTagoXmlData(wayLatitude,wayLongitude);
+                buslist.setText(data);
+//                new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        data = getTagoXmlData(wayLatitude,wayLongitude);
+//                        runOnUiThread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                //buslist.setText("test");
+//                            }
+//                        });
+//                    }
+//                }).start();
+            }
         });
+
 
     }//onCreate
 
@@ -142,5 +180,89 @@ public class MainActivity extends Activity {
             }
         }
     }
+
+//    public String getTagoXmlData(double lat, double lon){
+//        String lat_str = Double.toString(lat);
+//        String lon_str = Double.toString(lon);
+//        StringBuffer buffer=new StringBuffer();
+//        String str= edit.getText().toString();//EditText에 작성된 Text얻어오기
+//
+//        String queryUrl="http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getCrdntPrxmtSttnList?serviceKey="
+//                +"%2BaCrLa%2Fp1lfYP3wx954IxePqBKnfeZ8EC0pcOupGbRWhxUuOf5HW52ieQEZojO%2FEXE0ES1My6X68c50H4dWVLw%3D%3D"
+//                + "&numOfRows=10&pageNo=1&_type=xml"
+//                +"&gpsLati="+lat_str+"&gpsLong="+lon_str;
+//
+//        try {
+//            URL url= new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
+//            InputStream is= url.openStream(); //url위치로 입력스트림 연결
+//
+//            XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
+//            XmlPullParser xpp= factory.newPullParser();
+//            xpp.setInput( new InputStreamReader(is, "UTF-8") ); //inputstream 으로부터 xml 입력받기
+//
+//            String tag;
+//
+//            xpp.next();
+//            int eventType= xpp.getEventType();
+//
+//            while( eventType != XmlPullParser.END_DOCUMENT ){
+//                switch( eventType ){
+//                    case XmlPullParser.START_DOCUMENT:
+//                        buffer.append("파싱 시작...\n\n");
+//                        break;
+//
+//                    case XmlPullParser.START_TAG:
+//                        tag= xpp.getName();//테그 이름 얻어오기
+//
+//                        if(tag.equals("item")) ;// 첫번째 검색결과
+//                        else if(tag.equals("gpslati")){
+//                            buffer.append("lat : ");
+//                            xpp.next();
+//                            buffer.append(xpp.getText());//title 요소의 TEXT 읽어와서 문자열버퍼에 추가
+//                            buffer.append("\n"); //줄바꿈 문자 추가
+//                        }
+//                        else if(tag.equals("gpslong")){
+//                            buffer.append("long : ");
+//                            xpp.next();
+//                            buffer.append(xpp.getText());//category 요소의 TEXT 읽어와서 문자열버퍼에 추가
+//                            buffer.append("\n");//줄바꿈 문자 추가
+//                        }
+//                        else if(tag.equals("nodeid")){
+//                            buffer.append("nodeid :");
+//                            xpp.next();
+//                            buffer.append(xpp.getText());//description 요소의 TEXT 읽어와서 문자열버퍼에 추가
+//                            buffer.append("\n");//줄바꿈 문자 추가
+//                        }
+//                        else if(tag.equals("nodenm")){
+//                            buffer.append("nodenm :");
+//                            xpp.next();
+//                            buffer.append(xpp.getText());//telephone 요소의 TEXT 읽어와서 문자열버퍼에 추가
+//                            buffer.append("\n");//줄바꿈 문자 추가
+//                        }
+//                        else if(tag.equals("nodeno")){
+//                            buffer.append("nodeno :");
+//                            xpp.next();
+//                            buffer.append(xpp.getText());//address 요소의 TEXT 읽어와서 문자열버퍼에 추가
+//                            buffer.append("\n");//줄바꿈 문자 추가
+//                        }
+//                        break;
+//
+//                    case XmlPullParser.TEXT:
+//                        break;
+//
+//                    case XmlPullParser.END_TAG:
+//                        tag= xpp.getName(); //테그 이름 얻어오기
+//
+//                        if(tag.equals("item")) buffer.append("\n");// 첫번째 검색결과종료..줄바꿈
+//                        break;
+//                }
+//                eventType= xpp.next();
+//            }
+//        } catch (Exception e) {
+//            // TODO Auto-generated catch blocke.printStackTrace();
+//        }
+//        buffer.append("파싱 끝\n");
+//        return buffer.toString();//StringBuffer 문자열 객체 반환
+//    }//getXmlData method....
 
 }
---
+++ app/src/main/java/com/example/busapp/TagoThread.java
@@ -0,0 +1,135 @@
+package com.example.busapp;
+
+import android.widget.EditText;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class TagoThread extends Thread{
+    private EditText edit;
+    @Override
+    public void run(){
+        try {
+            StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getCrdntPrxmtSttnList"); /*URL*/
+            urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=%2BaCrLa%2Fp1lfYP3wx954IxePqBKnfeZ8EC0pcOupGbRWhxUuOf5HW52ieQEZojO%2FEXE0ES1My6X68c50H4dWVLw%3D%3D"); /*Service Key*/
+            urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+            urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+            urlBuilder.append("&" + URLEncoder.encode("_type","UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8")); /*데이터 타입(xml, json)*/
+            urlBuilder.append("&" + URLEncoder.encode("gpsLati","UTF-8") + "=" + URLEncoder.encode("36.3", "UTF-8")); /*WGS84 위도 좌표*/
+            urlBuilder.append("&" + URLEncoder.encode("gpsLong","UTF-8") + "=" + URLEncoder.encode("127.3", "UTF-8")); /*WGS84 경도 좌표*/
+            URL url = new URL(urlBuilder.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-type", "application/json");
+            System.out.println("Response code: " + conn.getResponseCode());
+            BufferedReader rd;
+            if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+            conn.disconnect();
+            System.out.println(sb.toString());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public String getTagoXmlData(double lat, double lon){
+        String lat_str = Double.toString(lat);
+        String lon_str = Double.toString(lon);
+        StringBuffer buffer=new StringBuffer();
+        //String str= edit.getText().toString();//EditText에 작성된 Text얻어오기
+
+        String queryUrl="http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getCrdntPrxmtSttnList?serviceKey="
+                +"%2BaCrLa%2Fp1lfYP3wx954IxePqBKnfeZ8EC0pcOupGbRWhxUuOf5HW52ieQEZojO%2FEXE0ES1My6X68c50H4dWVLw%3D%3D"
+                + "&numOfRows=10&pageNo=1&_type=xml"
+                +"&gpsLati="+lat_str+"&gpsLong="+lon_str;
+
+        try {
+            URL url= new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
+            InputStream is= url.openStream(); //url위치로 입력스트림 연결
+
+            XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
+            XmlPullParser xpp= factory.newPullParser();
+            xpp.setInput( new InputStreamReader(is, "UTF-8") ); //inputstream 으로부터 xml 입력받기
+
+            String tag;
+
+            xpp.next();
+            int eventType= xpp.getEventType();
+
+            while( eventType != XmlPullParser.END_DOCUMENT ){
+                switch( eventType ){
+                    case XmlPullParser.START_DOCUMENT:
+                        buffer.append("파싱 시작...\n\n");
+                        break;
+
+                    case XmlPullParser.START_TAG:
+                        tag= xpp.getName();//테그 이름 얻어오기
+
+                        if(tag.equals("item")) ;// 첫번째 검색결과
+                        else if(tag.equals("gpslati")){
+                            buffer.append("lat : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());//title 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("gpslong")){
+                            buffer.append("long : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());//category 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n");//줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("nodeid")){
+                            buffer.append("nodeid :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//description 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n");//줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("nodenm")){
+                            buffer.append("nodenm :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//telephone 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n");//줄바꿈 문자 추가
+                        }
+                        else if(tag.equals("nodeno")){
+                            buffer.append("nodeno :");
+                            xpp.next();
+                            buffer.append(xpp.getText());//address 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n");//줄바꿈 문자 추가
+                        }
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        tag= xpp.getName(); //테그 이름 얻어오기
+
+                        if(tag.equals("item")) buffer.append("\n");// 첫번째 검색결과종료..줄바꿈
+                        break;
+                }
+                eventType= xpp.next();
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch blocke.printStackTrace();
+        }
+        buffer.append("파싱 끝\n");
+        return buffer.toString();//StringBuffer 문자열 객체 반환
+    }//getXmlData method....
+
+}
