82fb6a6b55f9b53ca059cab597f1a397f74dedd3
vpfl95 <k_dh610@naver.com>
2022-08-07 23:59:18 +0900

    curved layout success

--- app/src/main/java/com/example/busapp/StopListActivity.java
+++ app/src/main/java/com/example/busapp/StopListActivity.java
@@ -6,9 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.view.InputDeviceCompat;
+import androidx.core.view.MotionEventCompat;
+import androidx.core.view.ViewConfigurationCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.wear.widget.WearableLinearLayoutManager;
 import androidx.wear.widget.WearableRecyclerView;
@@ -42,11 +48,30 @@ public class StopListActivity extends Activity {
 
         binding = ActivityStopListBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        CustomScrollingLayoutCallback customScrollingLayoutCallback = new CustomScrollingLayoutCallback();
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setEdgeItemsCenteringEnabled(true);
-        recyclerView.setLayoutManager(new WearableLinearLayoutManager(this));
+        recyclerView.setLayoutManager(new WearableLinearLayoutManager(this, customScrollingLayoutCallback));
+
+        //rotary input
+        recyclerView.setOnGenericMotionListener(new View.OnGenericMotionListener() {
+            @Override
+            public boolean onGenericMotion(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction()==MotionEvent.ACTION_SCROLL &&
+                        motionEvent.isFromSource(InputDeviceCompat.SOURCE_ROTARY_ENCODER)
+                ){
+                    float delta = -motionEvent.getAxisValue(MotionEventCompat.AXIS_SCROLL) *
+                            ViewConfigurationCompat.getScaledVerticalScrollFactor(
+                                    ViewConfiguration.get(view.getContext()),view.getContext()
+                            );
+                    view.scrollBy(0,Math.round(delta));
+                    return true;
+                }
+                return false;
+            }
+        });
 
         adapter = new StopAdapter();
 
@@ -55,17 +80,6 @@ public class StopListActivity extends Activity {
         longitude = intent.getDoubleExtra("longitude", 0);
         TagoThread tagoThread = new TagoThread();
         tagoThread.start();
-//        data = getTagoXmlData();
-        //Log.d("data",data);
-
-//        StringTokenizer st = new StringTokenizer(data,",");
-//        while(st.hasMoreTokens()){
-//            String id = st.nextToken();
-//            String nm = st.nextToken();
-//            adapter.addItem(new BusStop(id,nm));
-//        }
-
-        //recyclerView.setAdapter(adapter);
 
     }
 
@@ -150,77 +164,27 @@ public class StopListActivity extends Activity {
         }
     }
 
+    public class CustomScrollingLayoutCallback extends WearableLinearLayoutManager.LayoutCallback {
+        /** How much should we scale the icon at most. */
+        private static final float MAX_ICON_PROGRESS = 0.65f;
 
-//    public String getTagoXmlData() {
-//        String lat_str = Double.toString(latitude);
-//        String lon_str = Double.toString(longitude);
-//        StringBuffer buffer = new StringBuffer();
-//
-//        Thread t = new Thread(() -> {
-//
-//            String queryUrl = "http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getCrdntPrxmtSttnList?serviceKey="
-//                    + "%2BaCrLa%2Fp1lfYP3wx954IxePqBKnfeZ8EC0pcOupGbRWhxUuOf5HW52ieQEZojO%2FEXE0ES1My6X68c50H4dWVLw%3D%3D"
-//                    + "&numOfRows=10&pageNo=1&_type=xml"
-//                    + "&gpsLati=" + lat_str + "&gpsLong=" + lon_str;
-//            try {
-//                URL url = new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
-//                InputStream is = url.openStream(); //url위치로 입력스트림 연결
-//
-//                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-//                XmlPullParser xpp = factory.newPullParser();
-//                xpp.setInput(new InputStreamReader(is, "UTF-8")); //inputstream 으로부터 xml 입력받기
-//                String tag;
-//                xpp.next();
-//                int eventType = xpp.getEventType();
-//                while (eventType != XmlPullParser.END_DOCUMENT) {
-//                    switch (eventType) {
-//                        case XmlPullParser.START_DOCUMENT:
-//                            buffer.append("파싱 시작...\n\n");
-//                            break;
-//
-//                        case XmlPullParser.START_TAG:
-//                            tag = xpp.getName();//테그 이름 얻어오기
-//
-//                            if (tag.equals("item")) ;// 첫번째 검색결과
-//                            else if (tag.equals("nodeid")) {
-//                                //buffer.append("nodeid :");
-//                                xpp.next();
-//
-//                                //Log.d("ID",xpp.getText());
-//                                buffer.append(xpp.getText());//description 요소의 TEXT 읽어와서 문자열버퍼에 추가
-//                                buffer.append(",");//줄바꿈 문자 추가
-//                            } else if (tag.equals("nodenm")) {
-//                                //buffer.append("nodenm :");
-//                                xpp.next();
-//
-//                                //Log.d("NM",xpp.getText());
-//                                buffer.append(xpp.getText());//telephone 요소의 TEXT 읽어와서 문자열버퍼에 추가
-//                                //buffer.append("\n");//줄바꿈 문자 추가
-//                            }
-//                            break;
-//
-//                        case XmlPullParser.TEXT:
-//                            break;
-//
-//                        case XmlPullParser.END_TAG:
-//                            tag = xpp.getName(); //테그 이름 얻어오기
-//                            if (tag.equals("item")) buffer.append(",");// 첫번째 검색결과종료..줄바꿈
-//                            break;
-//                    }
-//                    eventType = xpp.next();
-//                }
-//            } catch (Exception e) {
-//                // TODO Auto-generated catch blocke.printStackTrace();
-//            }
-//            //buffer.append("파싱 끝\n");
-//        });
-//        try {
-//            t.start();
-//            t.join();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        return buffer.toString();//StringBuffer 문자열 객체 반환
-//    }
+        private float progressToCenter;
+
+        @Override
+        public void onLayoutFinished(View child, RecyclerView parent) {
+
+            // Figure out % progress from top to bottom
+            float centerOffset = ((float) child.getHeight() / 2.0f) / (float) parent.getHeight();
+            float yRelativeToCenterOffset = (child.getY() / parent.getHeight()) + centerOffset;
+
+            // Normalize for center
+            progressToCenter = Math.abs(0.5f - yRelativeToCenterOffset);
+            // Adjust to the maximum scale
+            progressToCenter = Math.min(progressToCenter, MAX_ICON_PROGRESS);
+
+            child.setScaleX(1 - progressToCenter);
+            child.setScaleY(1 - progressToCenter);
+        }
+    }
 
 }
